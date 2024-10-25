1cba30a0125c30b65621859f2b38042aef7bb325
BIMA <bimantara.hanumpraja@gmail.com>
2024-10-25 14:58:35 +0700

    update code

--- stream_pubsub_join.py
+++ stream_pubsub_join.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from typing import Tuple
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -10,6 +11,12 @@ args, beam_args = parser.parse_known_args()
 pipeline_options = PipelineOptions(beam_args)
 
 
+def show_data(data: Tuple):
+    _, result = data
+    text = f"Student name: {result.get('source1')[0]}\nStudent GPA: {result.get('source2')[0]}"
+    print(text)
+
+
 def main():
     with beam.Pipeline(options=pipeline_options) as p:
         source1 = (p
@@ -17,7 +24,7 @@ def main():
                     subscription="projects/sandbox-boxsand/subscriptions/pycon2024-sub")
                    | "Decode message" >> beam.Map(lambda msg: json.loads(msg.decode('utf-8')))
                    | "Prepare key-value pair" >> beam.Map(lambda kv: (kv.get("student_id"), kv.get("name")))
-                   | "Window 1" >> beam.WindowInto(FixedWindows(120))
+                   | "Window 1" >> beam.WindowInto(FixedWindows(20))
                    )
 
         source2 = (p
@@ -25,12 +32,12 @@ def main():
                     subscription="projects/sandbox-boxsand/subscriptions/pycon2024-2-sub")
                    | "Decode message 2" >> beam.Map(lambda msg: json.loads(msg.decode('utf-8')))
                    | "Prepare key-value pair 2" >> beam.Map(lambda kv: (kv.get("student_id"), kv.get("gpa")))
-                   | "Window 2" >> beam.WindowInto(FixedWindows(120))
+                   | "Window 2" >> beam.WindowInto(FixedWindows(20))
                    )
 
         join = ({"source1": source1, "source2": source2}
                 | beam.CoGroupByKey()
-                | beam.Map(print)
+                | beam.Map(show_data)
                 )
 
 
--- stream_windowing.py
+++ stream_windowing.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from typing import Tuple
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -10,15 +11,21 @@ args, beam_args = parser.parse_known_args()
 pipeline_options = PipelineOptions(beam_args)
 
 
+def show_data(data: Tuple):
+    product, total_price = data
+    text = f"Product: {product}\nTotal price: {total_price}"
+    print(text)
+
+
 def main():
     with beam.Pipeline(options=pipeline_options) as p:
         steps = (p
                  | beam.io.ReadFromPubSub(subscription="projects/sandbox-boxsand/subscriptions/pycon2024-sub")
                  | "Decode message" >> beam.Map(lambda msg: json.loads(msg.decode('utf-8')))
                  | "Prepare key-value pairs" >> beam.Map(lambda s: (s.get("product"), s.get("price")))
-                 | "2 Minutes window" >> beam.WindowInto(FixedWindows(120))
+                 | "10 seconds window" >> beam.WindowInto(FixedWindows(10))
                  | "Sum all prices" >> beam.CombinePerKey(sum)
-                 | "Show the output" >> beam.Map(print)
+                 | "Show the output" >> beam.Map(show_data)
                  )
 
 
